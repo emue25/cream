ba77b37e2068d627ae242aec8649c83d4afba499
emue25 <48172339+emue25@users.noreply.github.com>
2021-06-30 11:28:16 +0700

    Update DB10.sh

--- DB10.sh
+++ DB10.sh
@@ -327,8 +327,8 @@ chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/ssh restart
 /etc/init.d/dropbear restart
 /etc/init.d/stunnel4 restart
-service squid restart
-/etc/init.d/nginx restart
+etc/init.d/squid restart
+#/etc/init.d/nginx restart
 #/etc/init.d/openvpn restart
 rm -rf ~/.bash_history && history -c
 echo "unset HISTFILE" >> /etc/profile
