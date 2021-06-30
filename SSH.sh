4c856365ba0ac9d8c96a048ea1cf51519fe5c0c2
emue25 <48172339+emue25@users.noreply.github.com>
2021-06-30 10:48:03 +0700

    Update SSH.sh

--- SSH.sh
+++ SSH.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # mod by kopet mania
 # initialisasi var
-export DEBIAN_FRONTEND=noninteractive
-OS=`uname -m`;
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+# initialisasi var
+MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 
 # detail nama perusahaan
